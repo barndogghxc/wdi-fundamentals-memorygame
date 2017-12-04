70b70e9a498f200c0ab9b58867021bff8b8035b1
barndogghxc <barndogghxc@yahoo.com>
2017-12-04 15:01:37 -0500

    created game logic

--- js/main.js
+++ js/main.js
@@ -2,20 +2,22 @@ var cards = ["queen", "queen", "king", "king"];
 
 var cardsInPlay = [];
 
-var cardOne = cards[0];
+var checkForMatch = function() {
+	if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!");
+	}else {
+		alert("Sorry, try again.")
+	}
+	};
+};
 
-cardsInPlay.push(cardOne);
+var flipCard = function(cardId) {
+	console.log("User flipped "+cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+	checkForMatch();
+};
 
-console.log("User flipped "+cardOne);
-
-var cardTwo = cards[1];
-
-cardsInPlay.push(cardTwo);
-
-console.log("User flipped "+cardsInPlay[1]);
-
-if (cardsInPlay.length === 2) { 
-	if (cardsInPlay[0] === cardsInPlay[1]) {alert("You found a match!");}
-	else {alert("Sorry, try again.");}
-}
+flipCard(0);
+flipCard(2);
 
