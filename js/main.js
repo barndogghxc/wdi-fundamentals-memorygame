bb9e51ebf2567dc4de706612788ae071d037584b
barndogghxc <barndogghxc@yahoo.com>
2017-12-04 17:17:00 -0500

    created game logic

--- js/main.js
+++ js/main.js
@@ -41,14 +41,6 @@ var flipCard = function(cardId) {
 	console.log(cards[cardId].suit);
 };
 
-var createBoard = function() {
-	for (var i = 0; i < cards.length; i++) {
-		var cardElement = document.createElement("img");
-		cardElement.setAttribute("src", "images/back.png");
-		cardElement.setAttribute("data-id", i);
-		cardElement.addEventListener("click", flipCard);
-		document.getElementById('game-board').appendChild(cardElement);
-	}
-}
+flipCard(0);
+flipCard(2);
 
-createBoard();
